8d883f8d1728b9f5f139e39a94fc62ed0373ba3d
benoit-bremaud <benoit.bremaud@laplateforme.io>
2023-09-18 12:36:39 +0200

    feat(public): created 'public layout' component and integrated in app
    Created the 'public layout' component to serve as the layout for public elements. Integrated it into the application to connect various public components.

--- Tutos_faisons_le_point/cocktail/src/app/public/public-routing.module.ts
+++ Tutos_faisons_le_point/cocktail/src/app/public/public-routing.module.ts
@@ -6,21 +6,23 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CocktailComponent } from './cocktail/cocktail.component';
 import { ContactComponent } from './contact/contact.component';
+import { PlayoutComponent } from './playout/playout.component';
 
 // Définition des routes avec leur chemin et composant associé.
 const routes: Routes = [
-  // Redirection de l'URL racine ('') vers la page d'accueil ('home') lorsque l'URL est vide ('').
-  // Cela signifie que si l'utilisateur accède à l'URL racine, il sera redirigé vers la page d'accueil.
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  {
+    path: '', component: PlayoutComponent, children: [
 
-  // Route pour la page d'accueil avec le composant 'HomeComponent'.
-  { path: 'home', component: HomeComponent},   // Route pour la page d'accueil
+      { path: '', redirectTo: 'home', pathMatch: 'full'},
+      
+      { path: 'home', component: HomeComponent},
+      { path: 'cocktail', component: CocktailComponent},  
+      { path: 'contact', component: ContactComponent}
 
-  // Route pour la page des cocktails avec le composant 'CocktailComponent'.
-  { path: 'cocktail', component: CocktailComponent},  // Route pour la page des cocktails
-
-  // Route pour la page de contact avec le composant 'ContactComponent'.
-  { path: 'contact', component: ContactComponent}   // Route pour la page de contact
+    ]
+  }
+  
+  
 ];
 
 @NgModule({
